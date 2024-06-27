efa96780952c06ac386100e2eb623d67f5247fd7
zadvajr <danielzadva@gmail.com>
2024-06-27 23:36:27 +0100

    f-string

---
+++ format_strings.py
@@ -0,0 +1,11 @@
+#Python - Format Strings
+#F-strings
+#F-strings are a way to embed expressions inside string literals, using curly braces {}.
+#They were introduced in Python 3.6.
+#Here is an example:
+name = "Daniel"
+age = 25
+print(f"My name is {name} and I am {age} years old.") #output: My name is Daniel and I am 25 years old.
+
+#You can also use expressions inside the curly braces:
+print(f"2 + 2 = {2 + 2}") #output: 2 + 2 = 4
