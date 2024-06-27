05e26f5b7efe7ccd3f30faf080a66c3f9d0db478
zadvajr <danielzadva@gmail.com>
2024-06-28 00:06:11 +0100

    string methods

--- py_string_methods.py
+++ py_string_methods.py
@@ -72,3 +72,58 @@ print(x) # Output: True
 txt = "50800"
 x = txt.isdigit()
 print(x) # Output: True
+
+# 16. isidentifier() - Returns True if the string is an identifier
+txt = "Demo"
+x = txt.isidentifier()
+print(x) # Output: True
+
+# 17. islower() - Returns True if all characters in the string are lower case
+txt = "hello world!"
+x = txt.islower()
+print(x) # Output: True
+
+# 18. isnumeric() - Returns True if all characters in the string are numeric
+txt = "565543"
+x = txt.isnumeric()
+print(x) # Output: True
+
+# 19. isprintable() - Returns True if all characters in the string are printable
+txt = "Hello! Are you #1?"
+x = txt.isprintable()
+print(x) # Output: True
+
+# 20. isspace() - Returns True if all characters in the string are whitespaces
+txt = "   "
+x = txt.isspace()
+print(x) # Output: True
+
+# 21. istitle() - Returns True if the string follows the rules of a title
+txt = "Hello, And Welcome To My World!"
+x = txt.istitle()
+print(x) # Output: True
+
+# 22. isupper() - Returns True if all characters in the string are upper case
+txt = "THIS IS NOW!"
+x = txt.isupper()
+print(x) # Output: True
+
+# 23. join() - Joins the elements of an iterable to the end of the string
+myTuple = ("John", "Peter", "Vicky")
+x = "#".join(myTuple)
+print(x) # Output: John#Peter#Vicky
+
+# 24. ljust() - Returns a left justified version of the string
+txt = "banana"
+x = txt.ljust(20)
+print(x, "is my favorite fruit.") # Output: banana               is my favorite fruit.
+
+# 25. lower() - Converts a string into lower case
+txt = "Hello my FRIENDS"
+x = txt.lower()
+print(x) # Output: hello my friends
+
+# 26. lstrip() - Returns a left trim version of the string
+txt = "     banana     "
+x = txt.lstrip()
+print("of all fruits", x, "is my favorite") # Output: of all fruits banana      is my favorite
