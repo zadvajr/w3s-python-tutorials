4d94ada330d066c792f54097ccb30be1400d6077
zadvajr <danielzadva@gmail.com>
2024-06-27 17:55:38 +0100

    python strings

---
+++ python_strings.py
@@ -0,0 +1,16 @@
+#string in python are created by enclosing characters in quotes. Python allows for either pairs of single or double quotes.
+#you can display a string literal with the print() function
+print("Hello, World!")
+
+#assign string to a variable
+a = "hello"
+print(a)
+
+#multiline strings
+#you can assign a multiline string to a variable by using three quotes
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."""
+print(a)
+
+#strings are arrays
