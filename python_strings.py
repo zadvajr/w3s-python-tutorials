661fecfe46ac2817b21bd4992b5805ca66c3b6f7
zadvajr <danielzadva@gmail.com>
2024-06-27 21:57:09 +0100

    added len() and looping through string

--- python_strings.py
+++ python_strings.py
@@ -14,3 +14,17 @@ sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."""
 print(a)
 
 #strings are arrays
+#strings in python are arrays of bytes representing unicode characters
+#square brackets can be used to access elements of the string
+a = "Hello, World!"
+print(a[1]) #output: e
+
+#loop through string
+name = "daniel"
+for x in name:
+    print(x)
+
+#string length
+#to get the length of a string, use the len() function
+text = "this is a string text"
+print(len(text)) #output: 21
