5b16abbf9ecb46165bd5d9dcd11d19052646b5e5
zadvajr <danielzadva@gmail.com>
2024-06-28 15:06:13 +0100

    python list

---
+++ python_list.py
@@ -0,0 +1,23 @@
+#python list
+#list is a collection which is ordered and changeable. Allows duplicate members.
+#In Python lists are written with square brackets.
+#Example
+#Create a List:
+thislist = ["apple", "banana", "cherry"]
+print(thislist) # Output: ['apple', 'banana', 'cherry']
+
+#List length
+#To determine how many items a list has, use the len() function:
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist)) # Output: 3
+
+#List Items - Data Types
+#List items can be of any data type:
+#Example
+#A list with strings, integers and boolean values:
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+print(list1) # Output: ['apple', 'banana', 'cherry']
+print(list2) # Output: [1, 5, 7, 9, 3]
+print(list3) # Output: [True, False, False]
---
+++ word_list.py
@@ -0,0 +1,12 @@
+words_list = [
+    "soft", "aware", "of", "are", "area", "ear", "era", "engineer", "ring", "rate", "sat", 
+    "seat", "wear", "were", "we", "earn", "earning", "going", "near", "rear", "gin", "warn", 
+    "warning", "gin", "gene", "ore", "often", "one", "ones", "erring", "raw", "oft", "gearing", 
+    "gear", "engine", "software", "engineering", "sage", "sea", "rose", "rise", "rent", "rein", 
+    "rear", "sine", "sear", "ant", "eager", "ease", "east", "ate", "eat", "eater", "ewe", 
+    "game", "for", "gent", "giant", "free", "fore", "seen", "site", "tone", "tore", "wear", 
+    "wean", "wine", "wise", "write"
+]
+
+# Print the list to verify
+print(words_list)
