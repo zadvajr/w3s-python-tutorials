4248a1810506c0d0e8772e1f4ec50a9d1de60e16
zadvajr <danielzadva@gmail.com>
2024-06-29 00:08:41 +0100

    add any iterable

--- python_add_list_items.py
+++ python_add_list_items.py
@@ -15,3 +15,18 @@ print(fruits) #Prints the list of fruits: apple, banana, cherry
 fruits.insert(0, "orange") #Adds the fruit orange to the list at index 0
 print(fruits) #Prints the updated list of fruits: orange, apple, banana, cherry
 
+#Extend List
+#To add elements of another list to the current list, use the extend() method.
+#For Example:
+food = ["apple", "banana", "cherry"]
+meats = ["beef", "chicken", "pork"]
+food.extend(meats)
+print(food) #Prints the updated list of food: apple, banana, cherry, beef, chicken, pork
+
+#Add Any Iterable
+#The extend() method does not have to append lists, you can add any iterable object (tuples, sets, dictionaries etc.).
+#For Example:
+food = ["apple", "banana", "cherry"]
+tuple = ("beef", "chicken", "pork")
+food.extend(tuple)
+print(food) #Prints the updated list of food: apple, banana, cherry, beef, chicken, pork
