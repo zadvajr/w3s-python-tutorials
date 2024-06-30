89a525a3ff5dd7c5fcdd5c9ccdb045719ceb385a
zadvajr <danielzadva@gmail.com>
2024-06-30 19:55:32 +0100

    list methods

--- list_methods.py
+++ list_methods.py
@@ -98,3 +98,54 @@ del thislist
 thislist = ["apple", "banana", "cherry"]
 thislist.clear()
 print(thislist)
+
+#Copy a List
+#You cannot copy a list simply by typing list2 = list1, because: list2 will only be a reference to list1, and changes made in list1 will automatically also be made in list2.
+#There are ways to make a copy, one way is to use the built-in List method copy().
+#Example
+#Make a copy of a list with the copy() method:
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+#Another way to make a copy is to use the built-in method list().
+#Example
+#Make a copy of a list with the list() method:
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
+
+#Join Two Lists
+#There are several ways to join, or concatenate, two or more lists in Python.
+#One of the easiest ways are by using the + operator.
+#Example
+#Join two list:
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+list3 = list1 + list2
+print(list3)
+
+#Another way to join two lists are by appending all the items from list2 into list1, one by one:
+#Example
+#Append list2 into list1:
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+for x in list2:
+  list1.append(x)
+print(list1)
+
+#Or you can use the extend() method, which purpose is to add elements from one list to another list:
+#Example
+#Use the extend() method to add list2 at the end of list1:
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+list1.extend(list2)
+print(list1)
+
+#The list() Constructor
+#It is also possible to use the list() constructor to make a new list.
+#Example
+#Using the list() constructor to make a List:
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
