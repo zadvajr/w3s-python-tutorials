8aa9ee88974bce90fd206b84b1284ca739c061f2
zadvajr <danielzadva@gmail.com>
2024-07-01 13:54:49 +0100

    access tuple items

--- py_access_tuple_items.py
+++ py_access_tuple_items.py
@@ -17,3 +17,11 @@ print(thistuple[-1]) #Output: cherry
 #Range of Indexes
 #You can specify a range of indexes by specifying where to start and where to end the range.
 #When specifying a range, the return value will be a new tuple with the specified items.
+#Example
+#Return the third, fourth, and fifth item:
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5]) #Output: ('cherry', 'orange', 'kiwi')
+
+#Note: The search will start at index 2 (included) and end at index 5 (not included).
+
+#Range of Negative Indexes
